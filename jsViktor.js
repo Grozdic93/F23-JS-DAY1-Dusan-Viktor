f70aa5d759292b38c3137239e6624b0c644fd70c
Viktor Beckes <viktorb9772@gmail.com>
2023-02-01 11:22:36 +0100

    BasicDone

---
+++ jsViktor.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+let he = "I am Viktor,";
+let my = "and I am 25 years old"
+document.write(he + " " + my);
+
+
+names = ["viktor", "dusan", "jelena", "dragan", "aleksandar"];
+
+console.log(names[3])
+
+document.write(he + " " + my + " " + "my friend is" + " " + names[3]);
