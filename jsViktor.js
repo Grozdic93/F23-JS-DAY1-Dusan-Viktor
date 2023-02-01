428926b97e7573120ce77b0603aa169a3a2d92fb
Viktor Beckes <viktorb9772@gmail.com>
2023-02-01 11:33:49 +0100

    IntermediateDone

--- jsViktor.js
+++ jsViktor.js
@@ -10,3 +10,21 @@ names = ["viktor", "dusan", "jelena", "dragan", "aleksandar"];
 console.log(names[3])
 
 document.write(he + " " + my + " " + "my friend is" + " " + names[3]);
+
+
+
+fruite = ["apple", "banana", "kiwi"];
+console.log (fruite)
+
+fruite.push("orange");
+console.log (fruite);
+
+fruite.pop();
+console.log(fruite);
+
+
+let string = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+let ne = string.split("/");
+console.log(ne.join("\n"))
+
+document.write(string)
